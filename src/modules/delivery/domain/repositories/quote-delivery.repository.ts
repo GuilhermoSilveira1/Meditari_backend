5c613d8d43720f01fa6960c8acf7db78c07e5b05
Guilhermo Silveira <guilhermosilveira@gmail.com>
2026-09-14 12:28:06 -0300

    chore: prepare repository for FastAPI migration

--- src/modules/delivery/domain/repositories/quote-delivery.repository.ts
+++
@@ -1,7 +0,0 @@
-import { QuoteDelivery } from "../entities/quote-delivery.entity";
-
-export abstract class QuoteDeliveryRepository {
-  abstract save(
-      quote: QuoteDelivery,
-  ): Promise<void>;
-}
